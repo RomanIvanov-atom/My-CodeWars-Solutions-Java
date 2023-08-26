1b4a423d944404f602b30a709b24116d1ec9d6ac
Roman Ivanov <atom10121998@mail.ru>
2023-08-26 14:23:55 +0300

    deleting testNH and use junit instead + refactor test on junit

--- src/test/java/kata/kyu7/BasicCalculatorTest.java
+++ src/test/java/kata/kyu7/BasicCalculatorTest.java
@@ -1,8 +1,7 @@
 package kata.kyu7;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BasicCalculatorTest {
     @Test
